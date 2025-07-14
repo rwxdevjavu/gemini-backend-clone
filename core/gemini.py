e5498a300c0dd71ff7089ac1ef104c6a82111803
rwxdevjavu <aravindswaminathan2000@gmail.com>
2025-07-14 18:31:00 +0530

    fixing commits

--- core/gemini.py
+++ core/gemini.py
@@ -1,33 +1,18 @@
-from celery import Celery
-import redis
-import json
-import os
-from dotenv import load_dotenv
-from sqlalchemy.orm import sessionmaker
-from db import engine
 from google import genai
 import os
 from dotenv import load_dotenv
-from models.message import Message
 
 load_dotenv()
+from models.message import Message
 
 GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
-
-load_dotenv()
-
-REDIS_URL = os.getenv("REDIS_URL")
-celery_app = Celery("chatai", broker=REDIS_URL, backend=REDIS_URL)
-
-redis_store = redis.Redis.from_url(REDIS_URL, decode_responses=True)
-
-SessionLocal = sessionmaker(bind=engine)
-
-@celery_app.task(name="tasks.request_gemini")
 def request_gemini(contests:str,chatroom_id:int):
+    from sqlalchemy.orm import sessionmaker
+    from db import engine
+    SessionLocal = sessionmaker(bind=engine)
     db = SessionLocal()
     try:
         response = client.models.generate_content(model="gemini-2.5-flash",contents=contests)
@@ -39,3 +24,4 @@ def request_gemini(contests:str,chatroom_id:int):
         return "Something went wrong"
     finally:
         db.close()
+
